b09da08dd2283c5634a647275bda03bb307a06a5
EH0260 <jonathan.moralessalazar@externos-cl.cencosud.com>
2023-03-14 20:50:19 -0500

    operadores

---
+++ operadores.py
@@ -0,0 +1,28 @@
+'''
+Operadores de comparación
+>
+<
+>=
+<=
+==
+'''
+
+numero_1 = 4
+numero_2 = '4'
+
+print(numero_1 == numero_2)
+
+'''
+operadores aritméticos
++
+-
+*
+/
+=
+'''
+
+'''
+operadores lógicos
+&&
+||
+'''
