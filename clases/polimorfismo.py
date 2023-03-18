e241a273c283d3f79350ba2b60448e181e897abf
Jonathan Morales <blonder413@gmail.com>
2023-03-18 11:27:41 -0500

    polimorfismo

--- clases/polimorfismo.py
+++ clases/polimorfismo.py
@@ -1,7 +1,6 @@
 '''
-En este módulo se muestra un ejemplo de polimorfismo
+No existe polimorfismo en Python
 '''
-
 class Persona:
     def saludar(self):
         return 'hola soy una persona'
@@ -9,14 +8,21 @@ class Persona:
 class Cliente(Persona):
     pass
 
+class Helper:
+    pass
+
 def get_nombre(persona: Persona):
-    '''
+    """
     Como la clase cliente hereda de persona, 
     un objeto tipo cliente puede comportarse como una persona
-    '''
+    """
     return persona.saludar()
-
 
 cliente = Cliente()
 print(get_nombre(cliente))
-print(type(cliente))
+
+persona = Persona()
+print(get_nombre(persona))
+
+helper = Helper();
+#print(get_nombre(helper))
