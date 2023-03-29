a6385ec1a14e4c66de628d5adcacbc5c12c8cec1
EH0260 <jonathan.moralessalazar@externos-cl.cencosud.cl>
2023-03-29 18:30:14 -0500

    diccionarios

--- diccionarios.py
+++ diccionarios.py
@@ -5,8 +5,8 @@
 print('set'.center(20, '-'))
 usuario = {'jonathan', 'morales', '1985-04-13', 1.68}
 usuario.add('blonder413@gmail.com')
-usuario.discard('jonathan')     # Elimina un elemento
-usuario.remove('1985-04-13')    # Elimina un elemento
+usuario.discard('jonathan')     # Elimina un elemento sin lanzar excepción
+usuario.remove('1985-04-13')    # Elimina un elemento, lanza excepción si no encuentra el elemento
 print(usuario)
 print(type(usuario))
 # ---------------------------------------------------
@@ -17,9 +17,25 @@ persona = {
     'profesion': 'ingeniero de sistemas',
     'estatura': 1.68
     }
+persona['vivo'] = True
+persona.pop('fecha_nacimiento')
 print(persona)
 print(persona['nombre'])
+print(persona.get('estatura'))
 print(type(persona))
+for indice, value in persona.items():
+    print(f'key: {indice} - value {value}')
+
+for indice in persona.keys():
+    print(f'key: {indice}')
+
+for value in persona.values():
+    print(f'value {value}')
+
+print('id' in persona)
+
+persona.clear() # Elimina toda la información del diccionario
+del persona # Elimina la variable
 # ---------------------------------------------------
 print('lista de diccionarios'.center(50, '-'))
 contactos = [
