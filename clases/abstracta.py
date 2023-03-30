618d109533c17771b854722d11b655ea3e92ce8c
EH0260 <jonathan.moralessalazar@externos-cl.cencosud.cl>
2023-03-30 11:00:30 -0500

    clases abstractas

---
+++ clases/abstracta.py
@@ -0,0 +1,23 @@
+'''
+Las clases abstractas no se pueden instanciar
+Para definir una clase abstracta debe heredar de ABC (Abstract Base clase)
+'''
+from abc import ABC, abstractmethod
+
+class Abstracta(ABC):
+    @abstractmethod
+    def metodo_abstracto(self):
+        '''`
+        al agregar un método abstracto, la clase debe ser abstracta
+        Para definir el método abstracto debemos usar el decorador y no ponerle cuerpo
+        '''
+        pass
+
+class Hija(Abstracta):
+    def metodo_abstracto(self):
+        '''
+        Este método debe sobreescribirse ya que al ser abstractos en la clase padre es obligatorio
+        '''
+        return 'se debe definir por heredar de una clase abstracta'
+
+hija = Hija()
