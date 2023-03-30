6feaf3e7d2549edcee3a524a9102f72028c854b1
EH0260 <jonathan.moralessalazar@externos-cl.cencosud.cl>
2023-03-30 06:55:18 -0500

    main

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ def main():
     print('función principal')
 
 if __name__ == '__main__':
+    '''
+    Este bloque se ejecuta si ejecutamos este archivo directamente
+    Si importamos este archivo en otro no se ejecuta este bloque 
+    '''
     main()
