5a0d55932d24e7db045341e4c8dbfebd08af1f5f
EH0260 <jonathan.moralessalazar@externos-cl.cencosud.cl>
2023-04-01 15:31:09 -0500

    variables

--- variables.py
+++ variables.py
@@ -8,6 +8,9 @@ lista = ['jonathan', 1985, 1.68]
 diccionario = {}
 tupla = ()
 any = any
+binario = 0b1010    # debe empezar con 0b
+octal = 0o12        # debe empezar on 0o
+hexadecimal = 0xA   # debe empezar con 0x
 
 numero = int('413')
 print(type(nada))
@@ -18,3 +21,4 @@ print(type(lista))
 print(type(diccionario))
 print(type(tupla))
 print(type(any))
+print(type(hexadecimal))
