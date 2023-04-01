fa943bd40db6b91fca1548177cec4c32c264a0e5
EH0260 <jonathan.moralessalazar@externos-cl.cencosud.cl>
2023-04-01 15:54:19 -0500

    float

---
+++ variables_float.py
@@ -0,0 +1,9 @@
+a = 3.0
+a = float('413')
+a = float(810)
+a = float(3e5)
+a = 4.0 + 5 # cualquier operación que contenga un float su resultado será float
+print(f'{a:.2f}')
+
+a = float(3e-5)
+print(f'{a} = {a:.5f}')
