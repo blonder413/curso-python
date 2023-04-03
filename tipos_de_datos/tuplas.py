563b7a0345c1626ba185b860d91dcfbf2a8939dd
EH0260 <jonathan.moralessalazar@externos-cl.cencosud.cl>
2023-04-03 18:00:19 -0500

    tuplas

--- tipos_de_datos/tuplas.py
+++ tipos_de_datos/tuplas.py
@@ -1,11 +1,11 @@
 # son inmutables
 
 numeros = (1,2,3,4,5)
-
-print(type(numeros))
+numeros = 1,2,3,4,5
 
 for i in numeros:
     print(i)
 
 tupla = ('jonathan',) # si la tupla tiene un solo elemento debe tener una coma al final o será tomado como str
 print(type(tupla))
+
