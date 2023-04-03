6918f9d0d95308b7987350334523f962406b8cce
EH0260 <jonathan.moralessalazar@externos-cl.cencosud.cl>
2023-04-03 17:50:22 -0500

    funcion zip

---
+++ funcion_zip.py
@@ -0,0 +1,33 @@
+# print(dir(__builtins__))    # Listado de funcines disponibles
+numeros = [1,2,3]   # Puede sera una tupla
+letras = ['a', 'b', 'c']
+mezcla = zip(numeros, letras)    # Recibe valores iterables
+# print(list(mezcla)) # se consuma una vez que se usa
+print(tuple(mezcla))
+
+# iterar en paralelo
+for numero, letra in zip(numeros, letras):
+    print(f'Numero: {numero}, letra: {letra}')
+
+# separar un zip
+mezcla = [(1,'a'), (2, 'b'), (3,'c')]
+numeros, letras = zip(*mezcla)
+
+print(numeros, letras)
+
+# ordenar
+letras = ['j', 'b', 'y']
+numeros = [8, 10, 4]
+mezcla = zip(letras, numeros)
+
+print(sorted(zip(letras, numeros)))
+
+llaves = ['nombre', 'apellido']
+valores = ['bridyith', 'gonzález']
+diccionario = dict(zip(llaves, valores))    # crear un diccionario
+print(diccionario)
+
+llave = ['nombre']
+valor = ['yulian']
+diccionario.update(zip(llave, valor))   # Actualizar un valor de un diccionario
+print(diccionario)
