a699112a44cb1546c6657ffe629e139d5d7892a7
EH0260 <jonathan.moralessalazar@externos-cl.cencosud.cl>
2023-04-03 20:14:29 -0500

    set

---
+++ tipos_de_datos/set.py
@@ -0,0 +1,25 @@
+'''
+Los set son mutables
+Los set manejan tipos de datos inmutables
+Los set contienen valores únicos
+'''
+conjunto = {'Jonathan', 1.68, True}
+set_vacio = set()
+conjunto.add('blonder413@gmail.com')
+conjunto = set([4,13,8,10])
+conjunto2 = {1985,1986}
+conjunto.update(conjunto2)
+copia = conjunto2.copy()
+print(id(copia), id(conjunto2))
+print(conjunto2 is copia)
+
+set1 = {1,2,3,4,5}
+set2 = {0,2,4,6,8}
+set3 = {1,3,5,7,9}
+print(set2.union(set3))
+print(set1.difference(set2))
+print(set1.intersection(set2))
+print(set1.symmetric_difference(set3))  # Todos menos los comunes (opuesto a intersection)
+print(set1.issubset(set3))
+print(set1.issuperset(set2))
+print(set2.isdisjoint(set3)) # True si no tiene elementos comunes
