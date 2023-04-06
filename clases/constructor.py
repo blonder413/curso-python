b37cf8c1d374f65c6d1ba922bcd9f407f028300d
EH0260 <jonathan.moralessalazar@externos-cl.cencosud.cl>
2023-04-06 06:38:23 -0500

    constructores

--- clases/constructor.py
+++ clases/constructor.py
@@ -1,8 +1,16 @@
+'''
+En python no existe como en sí la sobrecarga de constructores
+'''
 class Persona:
     def __init__(self, nombre, correo):
         self.nombre = nombre
         self.correo = correo
+    @classmethod
+    def crear_persona_vacia(cls):
+        return cls(None, None) # Llamamos al constructor
 
 persona = Persona('jonathan', 'blonder413@gmail.com')
 print(persona.nombre)
 print(persona.correo)
+persona_vacia = Persona.crear_persona_vacia()
+print(persona_vacia.nombre)
