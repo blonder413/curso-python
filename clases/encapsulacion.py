4f5ca146f3c67a4db2f32e6d1cd62ad107e0728d
EH0260 <jonathan.moralessalazar@externos-cl.cencosud.cl>
2023-04-06 07:45:45 -0500

    encapsulacion

--- clases/encapsulacion.py
+++ clases/encapsulacion.py
@@ -5,7 +5,18 @@ class Persona:
     
     def get_correo(self):
         return self.__correo        # el __ indica que el atributo es privado
+class Cliente(Persona):
+    pass
+class Helper():
+    pass
 
 persona = Persona()
-print(persona._nombre)
-print(persona.get_correo())
+# print(persona._nombre)
+print(persona.__correo) # No puede llamar al atributo porque es privado
+# print(persona.get_correo())
+
+cliente = Cliente()
+print(cliente._nombre)
+# print(cliente.__correo) # No es posible llamarlo porque es privado
+helper = Helper()
+# print(helper._nombre) # no es posible llamado porque Helper no hereda de Persona
