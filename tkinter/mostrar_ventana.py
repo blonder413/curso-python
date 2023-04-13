417c58cd704fdc231c70d79ab29c5793f1b2fc7a
EH0260 <jonathan.moralessalazar@externos-cl.cencosud.cl>
2023-04-12 20:05:15 -0500

    posicion de la ventana

--- tkinter/mostrar_ventana.py
+++ tkinter/mostrar_ventana.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
 ventana = tk.Tk()
-ventana.geometry('800x600') # definir el tamaño de la ventana
+ventana.geometry('800x600+500+300') # definir el tamaño y la posición de la ventana
 ventana.title('BlonderApp')
 ventana.mainloop()  # Muestra la ventana
