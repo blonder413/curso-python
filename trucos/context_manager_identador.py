0508c820842ca18fdb1a36fc75a1cf1bab935660
EH0260 <jonathan.moralessalazar@externos-cl.cencosud.cl>
2023-05-04 18:46:29 -0500

    context manager with

---
+++ trucos/context_manager_identador.py
@@ -0,0 +1,22 @@
+class ContextManagerIdentador():
+    def __init__(self):
+        self.nivel = 0
+
+
+    def __enter__(self):
+        self.nivel += 1
+        return self
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.nivel -= 1
+
+
+    def imprimir(self, texto):
+        print('  '*self.nivel + texto)
+
+if __name__ == '__main__':
+    with ContextManagerIdentador() as identador:
+        identador.imprimir('hola blonder')
+        with identador:
+            identador.imprimir('bye blonder')
