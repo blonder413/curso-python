cc694afdd2baef1c21e17ab6449b30a20cbd4f0b
Jonathan Morales <blonder413@gmail.com>
2023-05-04 15:02:16 -0500

    print

--- print.py
+++ print.py
@@ -13,6 +13,10 @@ print("hola {} el número es {}".format(nombre, numero))
 print("hola {nombre} el número es {numero}".format(numero=810, nombre="br"))
 print("hola {1} el número es {0}".format(numero, nombre))
 
+print("-hola %s, el número es %d" %(nombre, numero))
+print("--hola %(nombre)s, el número es %(numero)d" %{'nombre':'blonder','numero':413})
+
+# interpolación literal o f string
 print(f'el número es {numero}')
 print(f'el número es {numero + 1}')
 print(nombre.center(50, '-'))
