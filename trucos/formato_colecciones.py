f7fa204958258429f586f11d5941b83bdf8178de
EH0260 <jonathan.moralessalazar@externos-cl.cencosud.cl>
2023-05-04 18:36:18 -0500

    formato colecciones

---
+++ trucos/formato_colecciones.py
@@ -0,0 +1,12 @@
+'''
+Dar formatos consistentes a diccionarios, listas o cualquier colección
+'''
+
+lista = ['juan', 'ana', 'luis']
+#formato consistente
+lista = [
+    'juan',
+    'ana',
+    'luis',
+]
+print(lista)
