86315ead9b53b855d9167a2760616dc74107dafc
blonder413 <blonder41@gmail.com>
2023-05-30 09:18:19 -0500

    closure

--- funciones/closure.py
+++ funciones/closure.py
@@ -2,15 +2,32 @@
 Una función que define otra función y la regresa
 la función interna puede acceder a las variables de la función externa
 '''
-def operacion(a,b):
+
+
+def operacion(a, b):
     def sumar():
         return a + b
+
     return sumar
+
 
 print(operacion(4, 13)())
 
+
 # Usando un lambda
-def operacion(a,b):
-    return lambda : a + b
+def operacion(a, b):
+    return lambda: a + b
+
 
 print(operacion(8, 10)())
+
+
+def sumar_valor():
+    def agregar(valor):
+        return valor + 4
+
+    return agregar
+
+
+closure_agregar = sumar_valor()(13)
+print(closure_agregar)
