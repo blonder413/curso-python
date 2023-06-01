4f74657df152812245fe233e07a1e2050fcd37c5
blonder413 <blonder41@gmail.com>
2023-06-01 09:59:08 -0500

    numpy

---
+++ modulos/modulo_numpy.py
@@ -0,0 +1,12 @@
+"""
+https://numpy.org/
+NumPy is the fundamental package for scientific computing with Python.
+Fundamental package for array computing in Python
+pip install numpy
+pip show numpy
+"""
+import numpy
+print(numpy.version.version)
+array = numpy.array([1, 2, 3, 4, 5])
+print(type(array))
+print(array*2)
