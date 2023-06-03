55ba459558bda1ed0007cc6ed0c2eca9bd0a7904
Jonathan Morales <blonder413@gmail.com>
2023-06-03 17:14:56 -0500

    modulo rich

---
+++ modulo_rich.py
@@ -0,0 +1,8 @@
+"""
+rich es un módulo que permite mejorar la salida en pantalla
+https://pypi.org/project/rich/
+pip install rich
+"""
+from rich import print
+
+print("[bold yellow]hola mundo[/bold yellow]")
