a260d0f2f19588a1e84942702a4413a020abff1b
blonder413 <blonder41@gmail.com>
2023-06-15 14:29:39 -0500

    type hints

---
+++ tipos_de_datos/type_hints.py
@@ -0,0 +1,19 @@
+"""
+Python es de tipado dinámico, de tal forma que no podemos indicar un tipo de dato
+Pero se recomienda indicarlo a modo documentativo
+"""
+
+nada: None = None
+cadena: str = 'blonder'
+flotante: float = 1.68
+boolean: bool = True    # True o False
+entero: int = 413
+rango: range = range(5)
+lista: list = ['blonder', 413, 1.68]
+diccionario: dict = {}
+tupla: tuple = ()
+cualquiera: any = any
+binario: bin = 0b1010    # debe empezar con 0b
+octal: oct = 0o12        # debe empezar on 0o
+hexadecimal: hex = 0xA   # debe empezar con 0x
+byte: bytes = b'texto'
