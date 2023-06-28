18036a0e639447095cbd5a83f0e5842f748a6e43
blonder413 <blonder41@gmail.com>
2023-06-28 09:37:05 -0500

    ciclos

--- ciclos/break.py
+++ ciclos/break.py
@@ -1,6 +1,11 @@
+"""
+break rompe el ciclo, evita que se ejecute el else
+"""
 cont = 0
 while(cont < 10):
     cont += 1
     if cont == 5:
         break
     print(cont)
+else:
+    print("fin del while")
--- ciclos/while.py
+++ ciclos/while.py
@@ -5,3 +5,5 @@ cont = 1
 while(cont <= numero):
     print(cont)
     cont += 1
+else:
+    print("fin del while")
