4af720603824f441355f5d95a1243a01a56588a8
blonder413 <blonder41@gmail.com>
2023-07-03 15:28:43 -0500

    modulo datetime

---
+++ modulos/modulo_datetime.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+ahora = datetime.now()
+print(ahora)
+print(ahora.year)
+print(ahora.month)
+print(ahora.day)
+print(ahora.hour)
+print(ahora.minute)
+print(ahora.second)
