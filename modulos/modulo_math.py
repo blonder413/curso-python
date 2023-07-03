78a4d7868b3631702867c934d5afcf84e97a8f3b
blonder413 <blonder41@gmail.com>
2023-07-03 15:25:14 -0500

    modulo math

--- modulos/modulo_math.py
+++ modulos/modulo_math.py
@@ -1,3 +1,15 @@
+"""
+el módulo math contiene operaciones matemáticas
+"""
 import math
+
 n = [4.13, 1, 2]
 print(math.fsum(n))
+
+potencia = math.pow(2, 3)
+print(potencia)
+
+raiz_cuadrada = math.sqrt(9)
+print(raiz_cuadrada)
+
+print(math.pi)
