b6dc726f4c63a26de8ecc7ea25a7eb3714c3cec9
blonder413 <blonder41@gmail.com>
2023-08-12 18:02:06 -0500

    escapar caracteres

---
+++ escapar_cadenas.py
@@ -0,0 +1,13 @@
+"""
+podemos usar el caracter barra invertida para simular una acción
+inserta un salto de línea, escapar un caractere, insertar un tabulador
+"""
+
+SALTO_DE_LINEA = "hola\nmundo"
+print(SALTO_DE_LINEA)
+
+TABULADOR = "hola\tmundo"
+print(TABULADOR)
+
+ESCAPAR = "Escapo las \"comillas\" para que muestren en el texto"
+print(ESCAPAR)
