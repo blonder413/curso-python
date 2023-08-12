226e28942c7a0416b2cee54a2d760f415ee54ab7
blonder413 <blonder41@gmail.com>
2023-08-12 17:54:42 -0500

    texto en varias líneas

---
+++ comentarios.py
@@ -0,0 +1,22 @@
+# comentario de una línea
+
+'''
+es una cadena
+pero puede funcionar
+como comentario de varias líneas
+'''
+
+TEXTO = '''
+Este es un ejemplo
+   de como se ve el texto
+      cuando usamos comentarios de varias líneas
+'''
+print(TEXTO)
+
+
+TEXTO_2 = ('Esto evita los espacios'
+    ' que pueden existir'
+    ' al principio de las líneas'
+)
+
+print(TEXTO_2)
