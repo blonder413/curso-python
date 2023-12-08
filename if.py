cd5e5f56137a1f5fc5f0f1cd1a67a97c7127c9e9
Jonathan Morales <blonder413@gmail.com>
2023-12-08 07:04:16 -0500

    sentencias en múltiples líneas

--- if.py
+++ if.py
@@ -7,3 +7,7 @@ elif numero_2 > numero_1:
     print(f'{numero_2} es mayor que {numero_1}')
 else:
     print('Son iguales')
+
+if numero_1 > numero_2 or \
+        numero_2 > numero_1:
+    print('son distintos')
