b2a13bc762f0f764dd5c20ef971da507b0867be0
Jonathan Morales <blonder413@gmail.com>
2024-05-24 14:32:44 -0500

    fechas: definir timezone

--- fechas.py
+++ fechas.py
@@ -1,4 +1,8 @@
-from datetime import datetime, date, time, timedelta
+"""
+Manejo de fechas en Python
+"""
+from datetime import datetime, date, timedelta
+import pytz
 
 ahora = datetime.now()  # 2023-03-15 11:57:12.318996
 
@@ -11,14 +15,18 @@ segundo = ahora.second
 
 # formtear fechas
 hoy = datetime.today()
-formato_date = '%Y-%m-%d'
-formato_datetime = '%Y-%m-%d %H:%M:%S'
-print(hoy.strftime(formato_date))
+FORMATO_DATE = '%Y-%m-%d'
+FORMATO_DATETIME = '%Y-%m-%d %H:%M:%S'
+print(hoy.strftime(FORMATO_DATETIME))
 
 # convertir string en fecha
-fecha = datetime.strptime('2023-03-15', formato_date)
+fecha = datetime.strptime('2023-03-15', FORMATO_DATE)
 
 # operaciones weeks, days, hours, minutes, seconds, miliseconds, microseconds
 fecha = date.today() # yyyy-mm-dd
 print(f'ayer: {fecha - timedelta(days = 1)}')
 print(f'día {datetime.weekday(fecha)}') # 0 - lunes
+
+HOY = datetime.now(pytz.timezone("America/Santiago"))
+FECHA = str(HOY.strftime(FORMATO_DATETIME))
+print(FECHA)
